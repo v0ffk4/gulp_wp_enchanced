d15ef331fb3c9604145368d090bef343ad62c876
Vladimir <v0ffk4@outlook.com>
2017-08-07 12:30:08 +0300

    Update typography

--- dev/_common/index.php
+++ dev/_common/index.php
@@ -1,5 +1,14 @@
 <?php get_header() ?>
-<h1>This is the index page</h1>
+<h1>This is header 1</h1>
+<h2>This is header 2</h2>
+<h3>This is header 3</h3>
+<h4>This is header 4</h4>
+<h5>This is header 5</h5>
+<h6>This is header 6</h6>
+<p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi adipisci, odit id tenetur a. Nobis sed cupiditate iure eligendi, voluptas ad accusamus aspernatur quidem excepturi assumenda distinctio esse fugit voluptatem.</span>
+<span>Ducimus id quia eaque sequi veniam illo molestiae nostrum, cum tempora iste vel soluta facilis amet quisquam esse nulla odio maiores culpa nesciunt! Nostrum quam odit ipsum, commodi quaerat maxime.</span>
+<span>Perferendis doloremque, officiis consequuntur quibusdam qui provident repellat cupiditate hic voluptatem possimus ab autem numquam? Beatae maxime, cupiditate soluta modi laudantium, nulla quisquam consectetur eos, vel harum, assumenda nam suscipit!</span>
+<span>Quas in deserunt enim, modi sint voluptatum debitis maiores obcaecati ratione nulla non iure beatae, natus numquam laboriosam qui asperiores, distinctio ipsum eos mollitia harum sequi nostrum. Veniam repellat, autem.</span>
+<span>Consectetur quia, aperiam officia distinctio. Eaque deleniti ipsum eius at mollitia placeat ea eum excepturi qui, pariatur adipisci ab atque molestias consectetur, illo, tempore, quisquam possimus doloribus ad quo laboriosam.</span></p>
 <?php get_template_part('contact'); ?>
-
 <?php get_footer() ?>
