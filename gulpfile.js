e2a95978c22e6e0d2f92facf1dd165e056dd4636
Vladimir <v0ffk4@outlook.com>
2017-08-12 11:24:58 +0300

    Switch pipeline to PUG template engine

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
 	rename = require('gulp-rename'),
 
+	//tpl processing
+	pug = require('gulp-pug'),
+
 	//css processing
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
 	cssnano = require('cssnano'),
-	precss = require('precss'),
 	stylus = require('gulp-stylus'),
 
 	//js processing
@@ -25,41 +27,67 @@ gulp.task('mediaCp', function() {
 		.pipe(gulp.dest(out + '/m'));
 	gulp.src('dev/_common/**/*.{png,jpg,svg}')
 		.pipe(rename({dirname:''}))
-		.pipe(gulp.dest(out))
+		.pipe(gulp.dest(out));
+	gulp.src('dev/_common/**/*.{ttf,otf,eot}')
+		.pipe(rename({dirname:''}))
+		.pipe(gulp.dest(out + '/f'))
 		.pipe(livereload());
 });
 
-//copy php
-gulp.task('tplCp', function() {
-	gulp.src('dev/**/*.{php,tpl,html}')
+//prepare templates
+gulp.task('tplPrep', function() {
+	gulp.src('dev/**/*.{html,tpl,php}')
 		.pipe(rename({dirname:''}))
 		.pipe(gulp.dest(out))
+	gulp.src('dev/**/*.pug')
+		.pipe(pug())
+		.pipe(rename({dirname:''}))
+		.pipe(rename({extname:'.php'}))
+		.pipe(gulp.dest(out))
 		.pipe(livereload());
 });
 
-//compile SASS synthax / minify
+//compile Stylus synthax / minify
 	gulp.task('cssPrep', function() {
-		gulp.src('dev/_common/style.styl')
+		gulp.src('dev/**/*.styl'),
+		gulp.src('dev/**/!(_)*.styl')
 			.pipe(stylus())
 			.pipe(postcss([
-				precss(),
 				autoprefixer(),
 				cssnano()
 		]))
 		.on('error', gutil.log)
+		.pipe(rename({
+			dirname:''
+		}))
 		.pipe(gulp.dest(out))
 		.pipe(livereload());
 	});
 
-	//concatinate & minify & rename javascript
-	gulp.task('jsConcat', function(){
+	//prepare javascript
+	gulp.task('jsPrep', function(){
 		gulp.src([
-			'dev/**/!(assembly)*.js',
-			'dev/_common/assembly.js'
+			'dev/**/_!(script)*.js',
+			'dev/_common/_script.js'
 		])
 			.pipe(concat('script.js'))
 			.pipe(uglify())
-			.pipe(rename({suffix: '.min'}))
+			.pipe(rename({
+				dirname:'',
+				suffix: '.min',
+				extname: '.js'
+			}))
+			.pipe(gulp.dest(out + '/j'));
+
+		gulp.src([
+			'dev/**/*.js',
+			'!dev/**/_*.js'
+		])
+			.pipe(uglify())
+			.pipe(rename({
+				dirname:'',
+				suffix: '.min'
+			}))
 			.pipe(gulp.dest(out + '/j'))
 			.pipe(livereload());
 	});
@@ -68,10 +96,10 @@ gulp.task('tplCp', function() {
 	gulp.task('watch', function() {
 		livereload.listen();
 		gulp.watch('dev/*/**.{png,jpg,svg}', ['mediaCp']);
-		gulp.watch('dev/**/*.{php,tpl,html}', ['tplCp']);
+		gulp.watch('dev/**/*.{html,php,tpl,pug}', ['tplPrep']);
 		gulp.watch('dev/**/*.styl', ['cssPrep']);
-		gulp.watch('dev/**/*.js', ['jsConcat']);
+		gulp.watch('dev/**/*.js', ['jsPrep']);
+//		gulp.watch('dev/**/*.include', ["liveReload"]);
 	});
 
-gulp.task( 'default', [ 'mediaCp', 'tplCp', 'cssPrep', 'jsConcat', 'watch' ] );
-//gulp.task( 'default', [ 'tplCp', 'cssPrep', 'jsConcat', 'watch' ] );
+gulp.task( 'default', [ 'mediaCp', 'tplPrep', 'cssPrep', 'jsPrep', 'watch' ] );
